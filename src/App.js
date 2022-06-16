8857ae4c40e82e5f911c514f4d9a310f845d0863
lautisaest <lautysaenzestrada@gmail.com>
2022-06-16 00:35:08 -0300

    adding home page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,12 @@
-import React, {useState} from 'react';
-import {navButton} from './components/nav-button'
+import React from 'react';
 import './App.css';
+//import Product from './components/Product';
+//import Navbar from './components/Navbar'
 
 function App() {
 
-  const clickManagement = () =>{
-    
-  }
-
-  var isLogged = false;
-
-  var buttonText;
-
-  isLogged ? buttonText='my account' : buttonText='login'
-
   return (
     <div classNameName='app'>
-      <nav className='navbar'>
-        <a className='navbar-brand' href='home'>E-commerce</a>
-        <a className='navbar-order' href='order'>orders</a> 
-        <form className='search-form'>
-          <input className='search-form-control' type='search' placeholder='Search' aria-label='Search' />
-          <button className='search-form-submit' type='submit'>Search</button>
-        </form>
-        <navButton 
-        buttonText
-        isLogged
-
-
-        ></navButton>
-      </nav>
-
-
-
-
     </div>
     
   );
